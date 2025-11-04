a777e03c38dc123308d62c8d86d5eef8b8b964ee
NIYOMUGISHA Obed <obed6@duck.com>
2025-11-04 21:59:50 +0200

    Add favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,8 @@
   import '@fontsource-variable/roboto';
   import '@fontsource/libre-caslon-text';
   import '$lib/app.css';
-  import favicon from '$lib/assets/favicon.svg';
 
   let { children } = $props();
 </script>
-
-<svelte:head>
-  <link rel="icon" href={favicon} />
-</svelte:head>
 
 {@render children()}
