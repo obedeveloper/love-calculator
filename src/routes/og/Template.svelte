195ca5f234f121287696faaffba257b0a5ca03a9
NIYOMUGISHA Obed <obed6@duck.com>
2025-11-04 20:07:50 +0200

    Create og route for shared links

---
+++ src/routes/og/Template.svelte
@@ -0,0 +1,60 @@
+<svelte:options css="injected" />
+
+<script lang="ts">
+  import calc from '$lib/calc';
+  import { randomLoveEmoji } from './utils';
+
+  interface Props {
+    firstName: string;
+    secondName: string;
+  }
+
+  const { firstName, secondName }: Props = $props();
+</script>
+
+<div>
+  <h1>{firstName}</h1>
+  <p>
+    <span class="emoji">{randomLoveEmoji()}</span>
+    <span class="percentage">{calc(firstName, secondName)}%</span>
+  </p>
+  <h1>{secondName}</h1>
+</div>
+
+<style>
+  div {
+    display: flex;
+    flex-direction: column;
+    background-color: white;
+    height: 100%;
+    padding: 1rem;
+  }
+
+  h1 {
+    font-family: 'inter';
+  }
+
+  p {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  span {
+    color: #570023;
+  }
+
+  h1,
+  .emoji {
+    font-size: 7.5rem;
+  }
+
+  .percentage {
+    font-size: 15rem;
+    font-family: 'ds-digital';
+  }
+
+  .emoji {
+    font-family: 'noto-emoji';
+  }
+</style>
