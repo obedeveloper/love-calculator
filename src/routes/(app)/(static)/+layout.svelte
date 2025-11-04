ecc1d3f85419df073f322d2300420c21d1d604a1
NIYOMUGISHA Obed <obed6@duck.com>
2025-11-04 21:21:14 +0200

    Create /shared route and add Open Graph to routes that needs it!

--- src/routes/(app)/(static)/+layout.svelte
+++ src/routes/(app)/(static)/+layout.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
+  import OG from '$lib/components/OG.svelte';
+
   const { children } = $props();
 </script>
 
-<svelte:head>
-  <title>Love Calculator</title>
-  <meta
-    name="description"
-    content="Welcome to our Love Percentage Calculator! Curious about how compatible you and your partner are? This fun tool calculates your love percentage based on your names. Remember, it's all in good spirit for entertainment!"
-  />
-</svelte:head>
+<OG
+  title="Love Calculator"
+  desc="Curious about how compatible you and your partner are? This fun tool calculates your love percentage based on your names. Remember, it's all in good spirit for entertainment!"
+></OG>
 
 {@render children()}
---
+++ src/routes/(app)/shared/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import OG from '$lib/components/OG.svelte';
+
+  const { data } = $props();
+  const { ogURL, title } = data;
+</script>
+
+<OG
+  {title}
+  desc="{title}! Remember, it's all in good spirit for entertainment!"
+  img={ogURL}
+></OG>
+
+<h1>{title}</h1>
+<img src={ogURL} alt="" />
+
+<style>
+  h1 {
+    font-size: 4rem;
+    text-align: center;
+    margin-block: 1.5rem;
+    text-decoration: underline;
+  }
+
+  img {
+    width: 100%;
+    margin-bottom: 2rem;
+    max-width: 62.5rem;
+    margin-inline: auto;
+    display: block;
+  }
+</style>
