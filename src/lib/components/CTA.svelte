e601c6ff3198fff3a983cabe5fec0d373a96d90c
NIYOMUGISHA Obed <obed6@duck.com>
2025-11-18 10:36:57 +0200

    Change CTA colors

--- src/lib/components/CTA.svelte
+++ src/lib/components/CTA.svelte
@@ -1,7 +1,7 @@
 <a
-	class="w-fit justify-self-center rounded bg-pink-500 px-6
+	class="w-fit justify-self-center rounded bg-pink-700 px-6
   py-2.5 text-2xl font-medium text-white transition-colors
-  duration-200 hover:bg-pink-600"
+  duration-200 hover:bg-pink-500"
 	href="/calc"
 >
 	Calculate Love!
