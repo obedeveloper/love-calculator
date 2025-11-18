8114e1f9678e4855b14c14e9ba3b65e77655562d
NIYOMUGISHA Obed <obed6@duck.com>
2025-11-18 19:31:56 +0200

    Refactor /shared and /embed

---
+++ src/routes/embed/Button.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	interface Props {
+		link: string;
+		text: string;
+		class?: string;
+	}
+	const { link, text, class: c }: Props = $props();
+</script>
+
+<a
+	class="rounded px-4 py-2 text-2xl
+  font-bold transition-colors {c}"
+	href={link.replace('og', 'shared')}
+>
+	{text}
+</a>
