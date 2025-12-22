8b7d9f0406926713cc1d4e1a51b95746f80275ce
NIYOMUGISHA Obed <obed6@duck.com>
2025-12-22 16:32:53 +0200

    Fix: update names on every keystroke

--- src/lib/components/calc/Form.svelte
+++ src/lib/components/calc/Form.svelte
@@ -3,21 +3,11 @@
 		firstName = $bindable(),
 		secondName = $bindable()
 	}: { firstName: string; secondName: string } = $props();
-
-	let fname = $derived(firstName);
-	let sname = $derived(secondName);
 </script>
 
 <form
-	onsubmit={(e) => {
-		e.preventDefault();
-
-		firstName = fname;
-		secondName = sname;
-	}}
 	class="grid gap-3 *:w-full *:focus:border-pink-500 *:focus:ring-pink-500 sm:grid-cols-2"
 >
-	<input type="text" bind:value={fname} placeholder="Eg: Romeo" />
-	<input type="text" bind:value={sname} placeholder="Eg: Juliet" />
-	<button hidden>Calculate</button>
+	<input type="text" bind:value={firstName} placeholder="Eg: Romeo" />
+	<input type="text" bind:value={secondName} placeholder="Eg: Juliet" />
 </form>
